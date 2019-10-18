49256692bb5c25a920a0650de799a300c1fda6d0
Willber Nascimento <willberufal@gmail.com>
2019-10-18 03:11:33 -0300

    first working version

--- backup.py
+++ backup.py
@@ -1,19 +1,48 @@
-import sys
-import os 
 
-local_dir = input("Diretório local: ")
-
-if os.path.exists(local_dir):
-    print("diretorio local existe")
-else:
-    print("inserir diretório localexistente")
-
-remot_dir = input("Diretório remoto: ")
-
-if os.path.exists(remot_dir):
-    print("remoto existe")
-else:
-    print("remoto não existe, tente novamente")
+import os
+from datetime import date
 
 
-os.system("rsync -Cravzp {} {}".format(local_dir, remot_dir))
+def main():
+    rsync_setup()
+
+
+def backup_setup():
+    while True:
+        dir_origin = input("Enter a origin path: ")
+        if not os.path.exists(dir_origin):
+            print("Enter a valid origin path.")
+            continue
+        break
+
+    while True:
+        dir_destine = input("Enter a destine path: ")
+        if not os.path.exists(dir_destine):
+            print("Destine path does not exists. Enter a valid path:")
+            continue
+        break
+
+    backup_name = input("Enter a backup name (if empty default is \"backup\"): ")
+    backup_date = date.today().strftime("%d-%m-%Y")
+
+    if backup_name == "":
+        backup_name = 'backup'
+    backup_name = backup_name + "-" + backup_date
+    backup_path = os.path.join(dir_destine, backup_name)
+    return dir_origin, backup_path
+
+    # print("Path origin: {}\n"
+    #       "Path destino: {}\n"
+    #       "Nome backup: {}\n"
+    #       "Path backup: {}".format(dir_origin, dir_destine, backup_name, backup_path))
+
+
+def rsync_setup():
+    opt_sync = "-Cravzp"
+    opt_exclude = "*.txt"
+    dir_origin, backup_path = backup_setup()
+    os.system("rsync {} --exclude {} {} {}".format(opt_sync, opt_exclude, dir_origin, backup_path))
+
+
+if __name__ == '__main__':
+    main()
